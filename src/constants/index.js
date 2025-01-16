2366359b03e02e655903e97e678644ca1fe14904
“apal7964@gmail.com” <apal794@gmail.com>
2025-01-16 13:07:12 +0530

    first-commit

---
+++ src/constants/index.js
@@ -0,0 +1,55 @@
+import project1 from "../assets/projects/6787ed03d9776_download.jpg";
+import project2 from "../assets/projects/6787edceed967_download.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+
+export const HERO_CONTENT = `I am a passionate frontend developer with a knack for crafting robust and scalable web applications. With 2 years of hands-on experience, I have honed my skills in front-end technologies like HTML, CSS, Javascript, React JS & Redux. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile frontend developer with a passion for creating efficient and user-friendly web applications. With 2 years of professional experience, I have worked with a variety of technologies, including HTML, CSS, Javascript, React JS, Tailwind CSS & Redux. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2021 - Present",
+    role: "Frontend Developer",
+    company: "Accenture",
+    description: `Skilled in building responsive and user-friendly web applications using HTML, CSS, Javascript, React JS, Tailwind CSS, and Redux. I have successfully developed and maintained web solutions that prioritize performance and user experience. Passionate about learning new technologies and collaborating with teams to deliver high-quality, scalable products.`,
+    technologies: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "React.js",
+      "Tailwind CSS",
+      "Redux",
+    ],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Password Generator",
+    image: project1,
+    description:
+      "A password generator project that creates strong, random passwords to enhance security and protect user data.",
+    technologies: ["HTML", "CSS", "Javascript", "React JS"],
+  },
+  {
+    title: "Netflix Clone",
+    image: project2,
+    description:
+      "A Netflix clone project is a streaming platform that replicates the user interface, content browsing, and video playback features of Netflix.",
+    technologies: ["HTML", "CSS", "React JS", "Redux", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "React JS", "Tailwind CSS", "Motion"],
+  },
+];
+
+export const CONTACT = {
+  address:
+    "9, Swagat Hsg Soc, Raje Shivaji Nagar, SV Road, Powai, Mumbai - 400072 ",
+  phoneNo: "+91 70 4547 7906",
+  email: "apal7964@gmail.com",
+};
